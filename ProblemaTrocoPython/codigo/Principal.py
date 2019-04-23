3a289a100228b58cc353faba394b89ac8b34d348
GabrielCavalcanteS <gabrielcavalcante1021@gmail.com>
2019-04-22 23:24:55 -0300

    Projeto completo.

--- ProblemaTrocoPython/codigo/Principal.py
+++ ProblemaTrocoPython/codigo/Principal.py
@@ -1,23 +1,31 @@
 from codigo.Troco import CalculaTroco
+
 '''
 Classe principal
 
 Classe onde os métodos da classe Troco são utilizados.
 '''
 
-class Principal:
 
+class Principal:
+    # Variável para a nova instância de CalculaTroco()
     cal = CalculaTroco()
+
     # Valor do troco.
-    valor_troco = 68
+    valor_troco = 49
+
     # Lista de moedas disponíveis. Baseada no Real (R$)
-    moedas_disponiveis = [100,50,25,10,5,1]
+    moedas_disponiveis = [100, 50, 25, 10, 5, 1]
     resp_incial = [0] * (valor_troco + 1)
-    contador=[0]*(valor_troco + 1)
+    contador = [0] * (valor_troco + 1)
 
     # Mostra a valor do troco em centavos.
     cal.mensagem_quantia(valor_troco)
+
     # Faz o cálculo inicial e mostra a quantidade de moedas.
-    cal.calcula_troco(moedas_disponiveis, valor_troco, contador,resp_incial)
+    cal.calcula_troco(moedas_disponiveis, valor_troco, contador, resp_incial)
+
     # Mostra as moedas utilizando a programação dinâmica.
     cal.caminho_dinamico(resp_incial, valor_troco)
+
+
